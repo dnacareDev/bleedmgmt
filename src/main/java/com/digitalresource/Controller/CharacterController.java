4bf3f7bf521a4848564d5a1621fddeeb5697d5a3
ddgg9511@naver.com <dddgg9511@github.com>
2021-11-24 18:01:23 +0900

    url 연결

--- src/main/java/com/digitalresource/Controller/CharacterController.java
+++ src/main/java/com/digitalresource/Controller/CharacterController.java
@@ -1,8 +1,14 @@
 package com.digitalresource.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class CharacterController {
@@ -11,5 +17,12 @@ public class CharacterController {
     public ModelAndView characterList(ModelAndView mv){
         mv.setViewName("resource/Character_list");
         return mv;
+    }
+
+    @PostMapping("search-character")
+    @ResponseBody
+    public Map<String, Object> searchCharacter(@RequestParam("sample_name") String sample_name, @RequestParam("page_num") int page_num, @RequestParam("limit") int limit){
+        Map<String,Object> map = new HashMap<>();
+        return map;
     }
 }
--- src/main/java/com/digitalresource/Controller/DataController.java
+++ src/main/java/com/digitalresource/Controller/DataController.java
@@ -1,8 +1,14 @@
 package com.digitalresource.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class DataController {
@@ -17,5 +23,12 @@ public class DataController {
     public ModelAndView registResourcePage(ModelAndView mv) {
         mv.setViewName("resource/regist_resource");
         return mv;
+    }
+
+    @PostMapping("search-resource")
+    @ResponseBody
+    public Map<String, Object> searchResource(@RequestParam("sample_name") String sample_name, @RequestParam("page_num") int page_num, @RequestParam("limit") int limit){
+        Map<String,Object> map = new HashMap<>();
+        return map;
     }
 }
