d24c027617559d4d9e2f0652a29b131a9aae51ec
yadon079 <zeidgame@gmail.com>
2021-12-16 19:14:38 +0900

    feature : 첨부파일 모달 창
    <description>
    - 첨부파일 업로드 작업 중
    <issue-number>

---
+++ src/main/java/com/digitalresource/Entity/BreedFile.java
@@ -0,0 +1,22 @@
+package com.digitalresource.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.ibatis.type.Alias;
+
+@Getter
+@Setter
+@ToString
+@Alias("BreedFile")
+public class BreedFile {
+  private int breed_file_id;
+  private int breed_id;
+  private String breed_file_title;
+  private String breed_file_contents;
+  private String create_date;
+  private String modify_date;
+
+  private String uploads_file;
+  private String uploads_origin_file;
+}
