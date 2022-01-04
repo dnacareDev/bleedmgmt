0d4cc7cde3dd997fe3887e6b69f9e0b42ab3065e
yadon079 <zeidgame@gmail.com>
2022-01-04 14:24:16 +0900

    feature : 오류 수정 중
    <description>
    - 데이터 삭제 오류 수정 중
      - breed_id를 잘못 가져오는 오류
    <issue-number>

--- src/main/java/com/digitalresource/Controller/BreedController.java
+++ src/main/java/com/digitalresource/Controller/BreedController.java
@@ -13,6 +13,9 @@ import java.util.*;
 
 import javax.servlet.http.HttpServlet;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,248 +32,250 @@ import java.util.Map;
 @Controller
 public class BreedController {
 
-	@Autowired
-	private CropService cropService;
+  @Autowired
+  private CropService cropService;
 
-	@Autowired
-	private ResourceNameService resourceNameService;
+  @Autowired
+  private ResourceNameService resourceNameService;
 
-	@Autowired
-	private ResourceService resourceService;
+  @Autowired
+  private ResourceService resourceService;
 
-	@Autowired
-	private DetailService detailService;
-	
-	@Autowired
-	private BreedService breedService;
+  @Autowired
+  private DetailService detailService;
 
-	@Autowired
-	private DataListService datalistService;
+  @Autowired
+  private BreedService breedService;
 
-	@Autowired
-	private FileController fileController;
+  @Autowired
+  private DataListService datalistService;
 
-	@RequestMapping("/breed")
-	public ModelAndView breed(ModelAndView mv, @RequestParam(value="type") String type, @RequestParam(value="id") int resource_id, @RequestParam(value="crop_id", required = false) int crop_id) {
-		Map<String, Object> result = new LinkedHashMap<String, Object>();
+  @Autowired
+  private FileController fileController;
 
-		List<Crop> crops = cropService.SearchCropList(type);
-	
-		mv.addObject("crop_id", crop_id);
-		mv.addObject("cropList", crops);
-		mv.addObject("type", type);
-		mv.addObject("resource_id", resource_id);
-		mv.setViewName("genome/breed");
-		
-		return mv;
-	}
+  @RequestMapping("/breed")
+  public ModelAndView breed(ModelAndView mv, @RequestParam(value = "type") String type, @RequestParam(value = "id") int resource_id, @RequestParam(value = "crop_id", required = false) int crop_id) {
+    Map<String, Object> result = new LinkedHashMap<String, Object>();
 
-	@ResponseBody
-	@RequestMapping("searchHeader")
-	public Map<String, Object> SearchBreed(Authentication auth, @RequestParam("crop_id") int crop_id, @RequestParam("resource_name") String resource_name) {
-		Map<String, Object> result = new LinkedHashMap<String, Object>();
-		int[] resourceNameId = resourceNameService.SelectResourceNameId(resource_name);
-		int resourceId = 0;
+    List<Crop> crops = cropService.SearchCropList(type);
 
-		for(int i = 0; i < resourceNameId.length; i++) {
-			if(resourceService.SearchResourceId(crop_id, resourceNameId[i]) != null) {
-				resourceId = resourceService.SearchResourceId(crop_id, resourceNameId[i]);
-				break;
-			}
-		}
+    mv.addObject("crop_id", crop_id);
+    mv.addObject("cropList", crops);
+    mv.addObject("type", type);
+    mv.addObject("resource_id", resource_id);
+    mv.setViewName("genome/breed");
 
-		List<Detail> details = detailService.SelectDetailListByResource(resourceId);	
-		List<Map<String, Object>> standardList = breedService.selectStandard(resourceId);
+    return mv;
+  }
 
-		String crop_name = breedService.SearchCropName(crop_id);
+  @ResponseBody
+  @RequestMapping("searchHeader")
+  public Map<String, Object> SearchBreed(Authentication auth, @RequestParam("crop_id") int crop_id, @RequestParam("resource_name") String resource_name) {
+    Map<String, Object> result = new LinkedHashMap<String, Object>();
+    int[] resourceNameId = resourceNameService.SelectResourceNameId(resource_name);
+    int resourceId = 0;
 
-		List<Breed> breed = breedService.SearchBreed(crop_name);
+    for (int i = 0; i < resourceNameId.length; i++) {
+      if (resourceService.SearchResourceId(crop_id, resourceNameId[i]) != null) {
+        resourceId = resourceService.SearchResourceId(crop_id, resourceNameId[i]);
+        break;
+      }
+    }
+
+    List<Detail> details = detailService.SelectDetailListByResource(resourceId);
+    List<Map<String, Object>> standardList = breedService.selectStandard(resourceId);
+
+    String crop_name = breedService.SearchCropName(crop_id);
+
+    List<Breed> breed = breedService.SearchBreed(crop_name);
+
+    result.put("breed", breed);
+    result.put("standardList", standardList);
+    result.put("detail", details);
+
+//    Gson gson = new Gson();
+//    String jsonString = gson.toJson(result.get("standardList"));
+//
+//    System.out.println(jsonString);
+//    System.out.println("================================");
+
+    return result;
+  }
+
+  @ResponseBody
+  @RequestMapping("updateStandardCell")
+  public int updateStandardCell(@RequestParam(value = "standard_data") String standard_data, @RequestParam(value = "standard_id") int standard_id) {
+    int result = 0;
+
+    StandardList standard = new StandardList();
+    standard.setStandard_data(standard_data);
+    standard.setStandard_id(standard_id);
+    result = breedService.updateStandardCell(standard);
+    return result;
+  }
 
 
-		result.put("breed", breed);
-		result.put("standardList",standardList);
-		result.put("detail", details);
+  @ResponseBody
+  @RequestMapping("insertBreed2")
+  public int insertBreed(@RequestParam(value = "data") String data, @RequestParam(value = "resource_id") int resource_id, @RequestParam(value = "crop_id") int crop_id, @RequestParam(value = "resource_name") String resource_name) {
+    int result = 0;
 
-		return result;
-	}
-	
-	@ResponseBody
-	@RequestMapping("updateStandardCell")
-	public int updateStandardCell(@RequestParam(value="standard_data") String standard_data,@RequestParam(value="standard_id") int standard_id) {
-		int result = 0;
-		
-		StandardList standard = new StandardList();
-		standard.setStandard_data(standard_data);
-		standard.setStandard_id(standard_id);
-		result = breedService.updateStandardCell(standard);
-		return result;
-	}
-	
-	
-	@ResponseBody
-	@RequestMapping("insertBreed2")
-	public int insertBreed(@RequestParam(value="data")String data, @RequestParam(value="resource_id") int resource_id, @RequestParam(value="crop_id")int crop_id, @RequestParam(value="resource_name") String resource_name) {
-		int result = 0;
+    System.out.println(data);
 
-		System.out.println(data);
+    result = breedService.insertBreed(resource_id, data, crop_id, resource_name);
 
-		result = breedService.insertBreed(resource_id,data,crop_id,resource_name);
+    return result;
+  }
 
-		return result;
-	}
-	
-	@ResponseBody
-	@RequestMapping("deleteBreed")
-	public int deleteBreed(String breed_id, String breed_row) {
-		System.out.println(breed_id);
-		System.out.println("breed_id ========================");
-		System.out.println(breed_row);
-		int result = breedService.deleteBreed(breed_id,breed_row);
-		return result;
-	}
+  @ResponseBody
+  @RequestMapping("deleteBreed")
+  public int deleteBreed(String breed_id) {
+    int result = breedService.deleteBreed(breed_id);
+    return result;
+  }
 
-	// 첨부파일 등록
-	@RequestMapping("insertBreedFile")
-	public ModelAndView InsertBreedFile(ModelAndView mv, @ModelAttribute BreedFile breed_file, @RequestParam("file") MultipartFile file) throws IOException {
-		String[] extension = file.getOriginalFilename().split("\\.");
+  // 첨부파일 등록
+  @RequestMapping("insertBreedFile")
+  public ModelAndView InsertBreedFile(ModelAndView mv, @ModelAttribute BreedFile breed_file, @RequestParam("file") MultipartFile file) throws IOException {
+    String[] extension = file.getOriginalFilename().split("\\.");
 
-		String file_name = fileController.ChangeFileName(extension[1]);
-		String origin_file_name = file.getOriginalFilename();
+    String file_name = fileController.ChangeFileName(extension[1]);
+    String origin_file_name = file.getOriginalFilename();
 
 //    String path = "src/main/webapp/upload";
-		String path = "/data/apache-tomcat-9.0.8/webapps/ROOT/upload/";
+    String path = "/data/apache-tomcat-9.0.8/webapps/ROOT/upload/";
 
-		File filePath = new File(path);
+    File filePath = new File(path);
 
-		if (!filePath.exists())
-			filePath.mkdirs();
+    if (!filePath.exists())
+      filePath.mkdirs();
 
-		Path fileLocation = Paths.get(path).toAbsolutePath().normalize();
-		Path targetLocation = fileLocation.resolve(file_name);
+    Path fileLocation = Paths.get(path).toAbsolutePath().normalize();
+    Path targetLocation = fileLocation.resolve(file_name);
 
-		Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+    Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 
-		int insert_file = breedService.InsertBreedFile(breed_file);
+    int insert_file = breedService.InsertBreedFile(breed_file);
 
-		Uploads upload = new Uploads();
-		upload.setUploads_file(file_name);
-		upload.setUploads_origin_file(origin_file_name);
-		upload.setBreed_file_id(breed_file.getBreed_file_id());
+    Uploads upload = new Uploads();
+    upload.setUploads_file(file_name);
+    upload.setUploads_origin_file(origin_file_name);
+    upload.setBreed_file_id(breed_file.getBreed_file_id());
 
-		int insert_upload = breedService.InsertBreedUpload(upload);
+    int insert_upload = breedService.InsertBreedUpload(upload);
 
-		mv.setViewName("redirect:/breed");
+    mv.setViewName("redirect:/breed");
 
-		return mv;
-	}
+    return mv;
+  }
 
-	@ResponseBody
-	@RequestMapping("searchBreed")
-	public Map<String, Object> SearchBreed(Authentication auth, @RequestParam("breed_name") String breed_name) {
-		Map<String, Object> result = new LinkedHashMap<String, Object>();
+  @ResponseBody
+  @RequestMapping("searchBreed")
+  public Map<String, Object> SearchBreed(Authentication auth, @RequestParam("breed_name") String breed_name) {
+    Map<String, Object> result = new LinkedHashMap<String, Object>();
 
-		User user = (User) auth.getPrincipal();
+    User user = (User) auth.getPrincipal();
 
-		int crop_id = Integer.parseInt(breed_name);
+    int crop_id = Integer.parseInt(breed_name);
 
-		String crop_name = breedService.SearchCropName(crop_id);
-		List<Breed> breed = breedService.SearchBreed(crop_name);                  // 품종 작물별 컬럼 조회
+    String crop_name = breedService.SearchCropName(crop_id);
+    List<Breed> breed = breedService.SearchBreed(crop_name);                  // 품종 작물별 컬럼 조회
 //		List<Display> display = service.SelectDisplay(user.getUser_id(), breed_name);          // 사용자별 품종 표시항목 조회
 //
 //		result.put("detail", detail);
 //		result.put("display", display);
-		result.put("breed", breed);
+    result.put("breed", breed);
 
-		return result;
-	}
+    return result;
+  }
 
-	@ResponseBody
-	@RequestMapping("excelBreed")
-	public int excelUpload(@RequestParam("excel_list") String excel_list, @RequestParam("resource_id") int resource_id) {
+  @ResponseBody
+  @RequestMapping("excelBreed")
+  public int excelUpload(@RequestParam("excel_list") String excel_list, @RequestParam("resource_id") int resource_id) {
 
-		JSONArray arr = new JSONArray(excel_list);
+    JSONArray arr = new JSONArray(excel_list);
 
-		List<StandardList> standards = new ArrayList<StandardList>();
+    List<StandardList> standards = new ArrayList<StandardList>();
 
-		for (int i = 0; i < arr.length(); i++) {
-			JSONArray item = arr.getJSONArray(i);
+    for (int i = 0; i < arr.length(); i++) {
+      JSONArray item = arr.getJSONArray(i);
 
-			int crop_id = Integer.parseInt(item.getString(0));
+      int crop_id = Integer.parseInt(item.getString(0));
 
-			String variety_name = breedService.SearchCropName(crop_id);
+      String variety_name = breedService.SearchCropName(crop_id);
 
-			Breed breed = new Breed();
-			breed.setVariety_name(variety_name);
-			breed.setResource_id(resource_id);
+      Breed breed = new Breed();
+      breed.setVariety_name(variety_name);
+      breed.setResource_id(resource_id);
 
 //			int breed_result = breedService.insertBreed(resource_id, data, crop_id, resource_name);
-			int breed_result = breedService.InsertBreed(breed);
+      int breed_result = breedService.InsertBreed(breed);
 
-			List<Detail> detail = breedService.SelectDetailExcel(resource_id);
+      List<Detail> detail = breedService.SelectDetailExcel(resource_id);
 
-			for (int j = 0; j < detail.size(); j++) {
-				StandardList standard = new StandardList();
+      for (int j = 0; j < detail.size(); j++) {
+        StandardList standard = new StandardList();
 
-				standard.setBreed_id(breed.getBreed_id());
-				standard.setDetail_id(detail.get(j).getDetail_id());
-				if(j < item.length()) {
-					standard.setStandard_data((String) item.get(j + 1));
-				} else {
-					standard.setStandard_data("");
-				}
+        standard.setBreed_id(breed.getBreed_id());
+        standard.setDetail_id(detail.get(j).getDetail_id());
+        if (j < item.length()) {
+          standard.setStandard_data((String) item.get(j + 1));
+        } else {
+          standard.setStandard_data("");
+        }
 
-				standards.add(standard);
-			}
-		}
-		breedService.InsertExcel(standards);
+        standards.add(standard);
+      }
+    }
+    breedService.InsertExcel(standards);
 
-		return 1;
-	}
+    return 1;
+  }
 
-	@ResponseBody
-	@RequestMapping("selectBreedDateGroup")
-	public Map<String, Object> SelectDateGroup(@RequestParam("resource_name") String resource_name) {
-		Map<String, Object> result = new LinkedHashMap<String, Object>();
+  @ResponseBody
+  @RequestMapping("selectBreedDateGroup")
+  public Map<String, Object> SelectDateGroup(@RequestParam("resource_name") String resource_name) {
+    Map<String, Object> result = new LinkedHashMap<String, Object>();
 
-		List<Map<String, String>> dataGroup = datalistService.SelectDateGroup(resource_name);
+    List<Map<String, String>> dataGroup = datalistService.SelectDateGroup(resource_name);
 
-		result.put("dataGroup", dataGroup);
+    result.put("dataGroup", dataGroup);
 
-		return result;
-	}
+    return result;
+  }
 
-	@ResponseBody
-	@RequestMapping("insertBreedDataList")
-	public DataList InsertDataList(@ModelAttribute DataList dataList, @RequestParam("listData") String listData) {
-		JSONArray arr = new JSONArray(listData);
+  @ResponseBody
+  @RequestMapping("insertBreedDataList")
+  public DataList InsertDataList(@ModelAttribute DataList dataList, @RequestParam("listData") String listData) {
+    JSONArray arr = new JSONArray(listData);
 
-		JSONObject obj = arr.getJSONObject(0);
+    JSONObject obj = arr.getJSONObject(0);
 
-		int crop_id = Integer.parseInt(obj.getString("crop_id"));
-		String variety_name = breedService.SearchCropName(crop_id);
+    int crop_id = Integer.parseInt(obj.getString("crop_id"));
+    String variety_name = breedService.SearchCropName(crop_id);
 
-		List<Breed> breed = breedService.SearchBreed(variety_name);
+    List<Breed> breed = breedService.SearchBreed(variety_name);
 
-		for (int i = 0; i < breed.size(); i++) {
-			if (Objects.equals(breed.get(i).getCreate_date().split(" ")[0], obj.getString("datalist_date"))) {
-				dataList.setResource_name(obj.getString("resource_name"));
-				dataList.setDatalist_date(obj.getString("datalist_date"));
-				dataList.setBreed_id(breed.get(i).getBreed_id());
-			} else {
-				continue;
-			}
+    for (int i = 0; i < breed.size(); i++) {
+      if (Objects.equals(breed.get(i).getCreate_date().split(" ")[0], obj.getString("datalist_date"))) {
+        dataList.setResource_name(obj.getString("resource_name"));
+        dataList.setDatalist_date(obj.getString("datalist_date"));
+        dataList.setBreed_id(breed.get(i).getBreed_id());
+      } else {
+        continue;
+      }
 
-			datalistService.InsertDataList(dataList);
-		}
+      datalistService.InsertDataList(dataList);
+    }
 
-		return dataList;
-	}
+    return dataList;
+  }
 
-	@ResponseBody
-	@RequestMapping("selectBreedStandard")
-	public List<StandardList> SelectBreedStandard(@RequestParam("breed_id") int breed_id) {
-		List<StandardList> result = breedService.SelectBreedStandard(breed_id);
+  @ResponseBody
+  @RequestMapping("selectBreedStandard")
+  public List<StandardList> SelectBreedStandard(@RequestParam("breed_id") int breed_id) {
+    List<StandardList> result = breedService.SelectBreedStandard(breed_id);
 
-		return result;
-	}
+    return result;
+  }
 }
--- src/main/java/com/digitalresource/ServiceImple/BreedServiceImpl.java
+++ src/main/java/com/digitalresource/ServiceImple/BreedServiceImpl.java
@@ -20,162 +20,172 @@ import com.digitalresource.Service.BreedService;
 @Service
 public class BreedServiceImpl implements BreedService {
 
-	@Autowired
-	private BreedMapper breedMapper;
-	 
-	@Override
-	public int insertBreed(int resource_id, String data,int crop_id, String resource_name) {
-		Map<String,Object> param = new HashMap<String, Object>();
-		Map<String,Object> map = new HashMap<String, Object>();
-		int result = 0;
-		param.put("crop_id", crop_id);
-		param.put("resource_name", resource_name);
-		resource_id = breedMapper.selectResourceId(param);
-		Breed breed = new Breed();
-		breed.setResource_id(resource_id);
-		// insert breed
-		int test = breedMapper.insertBreed(breed);
-		map.put("breed_id", breed.getBreed_id());
-		CountSelect countDetail = breedMapper.selectDetailCount(resource_id);
-		
-		// insert standard
-		JSONArray arr = new JSONArray(data);
-		int arrLength = arr.length();
-		int arrMin = 0;
-		String changeI = "";
-		int cnt = 0;
-		for(int i=countDetail.getMin(); i<=countDetail.getMax(); i++) {
-			if(arrMin != arrLength) {				
-				JSONObject jsonObject = arr.getJSONObject(arrMin);
-				changeI = Integer.toString(i);
-				if(changeI.equals(String.valueOf(jsonObject.get("detail_id")))) {
-					map.put("resource_id",resource_id);
-					map.put("limit",cnt);
-					map.put("standard_data", jsonObject.get("standard"));
-					arrMin++;
-				}else {
-					map.put("resource_id",resource_id);
-					map.put("limit",cnt);
-					map.put("standard_data", "");
-				}
-				
-			
-			}else {				
-					map.put("resource_id",resource_id);
-					map.put("limit",cnt);
-					map.put("standard_data", "");
-			}
-			result = breedMapper.insertStandard(map);
-			cnt++;
-		}
-		return result;
-	}
+  @Autowired
+  private BreedMapper breedMapper;
 
-	@Override
-	public int insertStandard() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+  @Override
+  public int insertBreed(int resource_id, String data, int crop_id, String resource_name) {
+    Map<String, Object> param = new HashMap<String, Object>();
+    Map<String, Object> map = new HashMap<String, Object>();
+    int result = 0;
+    param.put("crop_id", crop_id);
+    param.put("resource_name", resource_name);
+    resource_id = breedMapper.selectResourceId(param);
+    Breed breed = new Breed();
+    breed.setResource_id(resource_id);
+    // insert breed
+    int test = breedMapper.insertBreed(breed);
+    map.put("breed_id", breed.getBreed_id());
+    CountSelect countDetail = breedMapper.selectDetailCount(resource_id);
 
-	@Override
-	public int updateStandardCell(StandardList data) {
-		int result = 0;
-		result = breedMapper.updateStandardCell(data);
-		return result;
-	}
-	
-	
-	@Override
-	public List<Map<String, Object>> selectStandard(int resourceId) {
-		
-		List<StandardList> list =  breedMapper.selectStandard(resourceId);
-		List<Map<String, Object>> bodyList = new ArrayList<Map<String, Object>>();
-		Map<String , Object> dataMap = new HashMap<String , Object>();
-		int idx = 0;
-		int breed_id = 0;
-		int detail_id = 0;
-		int breed_row = 0;
-		// System.out.println(list.get(0).getBreed_row());
-		if(list.size() != 0 ) {			
-			breed_row = list.get(0).getBreed_row();		
-			idx=1;
-		}
-			for(StandardList item : list) {
-				if(item.getBreed_row() == breed_row) {
-					dataMap.put(String.valueOf(item.getDetail_id()) , item);
+    // insert standard
+    JSONArray arr = new JSONArray(data);
+    int arrLength = arr.length();
+    int arrMin = 0;
+    String changeI = "";
+    int cnt = 0;
+    for (int i = countDetail.getMin(); i <= countDetail.getMax(); i++) {
+      if (arrMin != arrLength) {
+        JSONObject jsonObject = arr.getJSONObject(arrMin);
+        changeI = Integer.toString(i);
+        if (changeI.equals(String.valueOf(jsonObject.get("detail_id")))) {
+          map.put("resource_id", resource_id);
+          map.put("limit", cnt);
+          map.put("standard_data", jsonObject.get("standard"));
+          arrMin++;
+        } else {
+          map.put("resource_id", resource_id);
+          map.put("limit", cnt);
+          map.put("standard_data", "");
+        }
 
-				}else {
-					dataMap.put("breed_row", item.getBreed_row());
-					dataMap.put("breed_id", item.getBreed_id());
-					dataMap.put("detail_id", item.getDetail_id());
-					dataMap.put("idx", idx++);
-					bodyList.add(dataMap);
-					
-					breed_row = item.getBreed_row();
-					breed_id = item.getBreed_id();
-					detail_id = item.getDetail_id();
-					dataMap = new HashMap<String , Object>();
-					dataMap.put(String.valueOf(item.getDetail_id()) , item);
-				}
-			}
-			dataMap.put("idx", idx++);
-			dataMap.put("breed_row", breed_row);
-			dataMap.put("breed_id", breed_id);
-			dataMap.put("detail_id", detail_id);
-			bodyList.add(dataMap);
-		return bodyList;
-	}
 
-	@Override
-	public int deleteBreed(String breed_id, String breed_row) {
+      } else {
+        map.put("resource_id", resource_id);
+        map.put("limit", cnt);
+        map.put("standard_data", "");
+      }
+      result = breedMapper.insertStandard(map);
+      cnt++;
+    }
+    return result;
+  }
 
-		int deleteResult = breedMapper.deleteStandard(breed_id);
-		int result = 0;
-		if(deleteResult != 0) {
-			breedMapper.deleteStandards(breed_id);
-			result = breedMapper.deleteBreed(breed_id);
-			System.out.println(result);
-		}
-		return result;
-	}
+  @Override
+  public int insertStandard() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
 
-	@Override
-	public int InsertBreedFile(BreedFile breed_file) {
-		return breedMapper.InsertBreedFile(breed_file);
-	}
+  @Override
+  public int updateStandardCell(StandardList data) {
+    int result = 0;
+    result = breedMapper.updateStandardCell(data);
+    return result;
+  }
 
-	@Override
-	public int InsertBreedUpload(Uploads upload) {
-		return breedMapper.InsertBreedUpload(upload);
-	}
+  @Override
+  public List<Map<String, Object>> selectStandard(int resourceId) {
+    List<StandardList> list = breedMapper.selectStandard(resourceId);
+    List<Map<String, Object>> bodyList = new ArrayList<Map<String, Object>>();
+    Map<String, Object> dataMap = new HashMap<String, Object>();
+//    int idx = 0;
+    int breed_id = 0;
+    int detail_id = 0;
+    int breed_row = 0;
 
-	@Override
-	public List<Breed> SearchBreed(String breed_name) {
-		return breedMapper.SearchBreed(breed_name);
-	}
+    if (list.size() != 0) {
+      breed_row = list.get(0).getBreed_row();
+//      idx = 1;
+    }
 
-	@Override
-	public String SearchCropName(int breed_name) {
-		return breedMapper.SearchCropName(breed_name);
-	}
+    int count = 0;
 
-	@Override
-	public int InsertBreed(Breed breed) {
-		return breedMapper.InsertBreed(breed);
-	}
+    for (StandardList item : list) {
+      System.out.println("count : " + count);
 
-	@Override
-	public List<Detail> SelectDetailExcel(int resource_id) {
-		return breedMapper.SelectDetailExcel(resource_id);
-	}
+      if (item.getBreed_row() == breed_row) {
+        System.out.println("breed_row : " + breed_row);
+        System.out.println("item.getBreed_row() : " + item.getBreed_row());
 
-	@Override
-	public int InsertExcel(List<StandardList> standards) {
-		return breedMapper.InsertExcel(standards);
-	}
+        dataMap.put(String.valueOf(item.getDetail_id()), item);
+      } else {
+        dataMap.put("breed_row", item.getBreed_row());
+        dataMap.put("breed_id", item.getBreed_id() - 1);
+        dataMap.put("detail_id", item.getDetail_id());
+//        dataMap.put("idx", idx++);
+        bodyList.add(dataMap);
 
-	@Override
-	public List<StandardList> SelectBreedStandard(int breed_id) {
-		return breedMapper.SelectBreedStandard(breed_id);
-	}
+        breed_row = item.getBreed_row();
+        breed_id = item.getBreed_id();
+        detail_id = item.getDetail_id();
+        dataMap = new HashMap<String, Object>();
+        dataMap.put(String.valueOf(item.getDetail_id()), item);
+      }
+
+      count++;
+    }
+
+//    dataMap.put("idx", idx++);
+    dataMap.put("breed_row", breed_row);
+    dataMap.put("breed_id", breed_id);
+    dataMap.put("detail_id", detail_id);
+
+    bodyList.add(dataMap);
+
+    return bodyList;
+  }
+
+  @Override
+  public int deleteBreed(String breed_id) {
+
+    int deleteResult = breedMapper.deleteStandard(breed_id);
+    int result = 0;
+    if (deleteResult != 0) {
+      breedMapper.deleteStandards(breed_id);
+      result = breedMapper.deleteBreed(breed_id);
+      System.out.println(result);
+    }
+    return result;
+  }
+
+  @Override
+  public int InsertBreedFile(BreedFile breed_file) {
+    return breedMapper.InsertBreedFile(breed_file);
+  }
+
+  @Override
+  public int InsertBreedUpload(Uploads upload) {
+    return breedMapper.InsertBreedUpload(upload);
+  }
+
+  @Override
+  public List<Breed> SearchBreed(String breed_name) {
+    return breedMapper.SearchBreed(breed_name);
+  }
+
+  @Override
+  public String SearchCropName(int breed_name) {
+    return breedMapper.SearchCropName(breed_name);
+  }
+
+  @Override
+  public int InsertBreed(Breed breed) {
+    return breedMapper.InsertBreed(breed);
+  }
+
+  @Override
+  public List<Detail> SelectDetailExcel(int resource_id) {
+    return breedMapper.SelectDetailExcel(resource_id);
+  }
+
+  @Override
+  public int InsertExcel(List<StandardList> standards) {
+    return breedMapper.InsertExcel(standards);
+  }
+
+  @Override
+  public List<StandardList> SelectBreedStandard(int breed_id) {
+    return breedMapper.SelectBreedStandard(breed_id);
+  }
 }
